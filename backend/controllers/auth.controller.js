9ea445a24e28e9d36fba79b0d570935e80422b3e
Praveen-077 <kumarp3986@gmail.com>
2024-11-03 13:11:39 +0530

    Commit1 Setting the backend MVC,Database connection

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const signup = async (req, res) => {
+  res.json({
+    data: "visited Signup page",
+  });
+};
+export const login = async (req, res) => {
+  res.json({
+    data: "visited login page",
+  });
+};
+export const logout = async (req, res) => {
+  res.json({
+    data: "visited logout page",
+  });
+};
