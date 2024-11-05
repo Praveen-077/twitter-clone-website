1b65b787096a8810035cd7c9f3ef10286b1181cf
Praveen-077 <kumarp3986@gmail.com>
2024-11-05 18:34:51 +0530

    commit 3 Added User Routes and User Controller,Notification. Cloudinary API

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import { getUserProfile,followUnfollowUser,getSuggestedUsers,updateUser } from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+router
+  .get("/profile/:username", protectRoute, getUserProfile)
+  .get("/suggested", protectRoute, getSuggestedUsers)
+  .post("/follow/:id",protectRoute, followUnfollowUser)
+  .post("/update", protectRoute, updateUser);
+
+export default router;
