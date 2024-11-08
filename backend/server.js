91ff68bcac6b5ccf7018f648bf2b433f7773075b
Praveen-077 <kumarp3986@gmail.com>
2024-11-08 16:14:26 +0530

    commit 11 Payload for image upload updated in server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); //for parsing req.body
+app.use(express.json({limit:"5mb"})); //for parsing req.body
 app.use(express.urlencoded({ extended: true })); //to parse form data
 app.use(cookieParser());
 
