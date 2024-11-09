4620b835011eb5ea5cd7181cf912e14a7595a10c
Praveen-077 <kumarp3986@gmail.com>
2024-11-09 11:11:31 +0530

    prepared app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import {v2 as cloudinary} from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
-import postRoutes from "./routes/post.routes.js"
-import notificationRoutes from "./routes/notification.routes.js"
+import postRoutes from "./routes/post.routes.js";
+import notificationRoutes from "./routes/notification.routes.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 dotenv.config();
@@ -14,12 +15,13 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-})
+});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use(express.json({limit:"5mb"})); //for parsing req.body
+app.use(express.json({ limit: "5mb" })); //for parsing req.body
 app.use(express.urlencoded({ extended: true })); //to parse form data
 app.use(cookieParser());
 
@@ -27,6 +29,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
